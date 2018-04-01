67c1599b04e3ea7da460e511a96c17903b78d820
H Graham <wookiegraham@gmail.com>
2018-04-01 11:36:26 -0500

    Created main.cpp
    Created main.cpp to hold the core of the pad control.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class abortSequence{
+
+	public:
+		#setMethods
+		#getMethods
+	private:
+
+		#declare methods here.
+}
+int main()
+{
+
+	return 0;
+}
