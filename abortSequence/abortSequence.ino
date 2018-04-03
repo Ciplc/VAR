5ad911630756171d6f4d97537e9159f59c4fcbae
Henry Graham <wookiegraham@gmail.com>
2018-04-03 16:41:31 -0500

    Basic abort
    Connect button to pin 2, press button toggle led and abort will be triggered.

---
+++ abortSequence/abortSequence.ino
@@ -0,0 +1,23 @@
+int ledPin = 13;
+int abortButton = 2;
+int abortState = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(abortIn, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  abortState = digitalRead(abortButton);
+
+  if (abortState == HIGH) {
+
+    digitalWrite(ledPin, HIGH)
+  } else {}
+
+  
+}
