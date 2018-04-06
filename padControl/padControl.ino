a0d1d10f3a39f313c4616c1476ba5ad6295c2695
Henry Graham <wookiegraham@gmail.com>
2018-04-06 13:11:12 -0500

    V .22
    Added abort IF statement, preliminary abort();. Not sure if we want to stop the countdown then throw error codes then end the program loop later.

--- padControl/padControl.ino
+++ padControl/padControl.ino
@@ -1,9 +1,27 @@
+#include <SevSeg.h>
+SevSeg sevseg; //Display Controller
+
+//Pin Variables
+int abortButton = A0;
+int ledPin = 13;
+
+//Variables
+int abortState;
+
 void setup() {
   // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+ abortState = digitalRead(abortButton);
+
+ if(abortState == HIGH){
+
+  digitalWrite(ledPin, HIGH);
+  //Write to single display with error code.
+  abort();
+  
+ }
 }
