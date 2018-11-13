49399764f05fa471dbdbf5871352d9e737e1450a
Ciplc <wookiegraham@gmail.com>
2018-11-12 19:35:11 -0600

    Updated datawrite.py to fit the project reqs Build not confirmed with ERPI or RPI

--- PS/datawrite.py
+++ PS/datawrite.py
@@ -9,39 +9,94 @@ import time
 sense = SenseHat()
 
 #config IMU sensor https://pythonhosted.org/sense-hat/api/#imu-sensor
-sense.set_imu_config(False, False, True)
+sense.set_imu_config(True, True, True)
 
-#get data from the SenseHat
-def get_data():
+#get environmental data from the sensehat
+def get_enviro():
 
-    pressure = sense.get_pressure()
-    temperature = sense.get_temperature()
-    accelraw = sense.get_accelerometer_raw()
 
-    #accelraw is in the format z,y,x in Gs
-    accelx = accelraw['x']
-    accely = accelraw['y']
+	pressure = sense.get_pressure()
+	tempPress = sense.get_temperature_from_pressure()
+	tempHumid = sense.get_temperature_from_humidity()
+	humidity = sense.get_humidity()
+
+	enviro_results = [
+
+		pressure,
+		tempPress,
+		tempHumid,
+		humidity
+
+	]
+
+	return enviro_results
+#get IMU data from the sensehat
+
+def get_IMU():
+
+	accelraw = sense.get_accelerometer_raw()
+
+	accelx = accelraw['x']	#change data into a friendlier format
+    accely = accelraw['y']	
     accelz = accelraw['z']
 
-    #change results into a more friendly format for writing
-    results = [
+	gyroraw = sense.get_gyroscope_raw()
 
-        time,
-        pressure,
-        temperature,
-        accelz,
-        accely,
-        accelx
+	gyrox = gyroraw['x']	#change data into a friendlier format
+	gyroy = gyroraw['y']
+    gyroz = gyroraw['z']
+
+    compassraw = sense.get_compass_raw()
+
+    compassx = compassraw['x']	#change data into a friendlier format
+	compassy = compassraw['y']
+    compassz = compassraw['z']
+
+
+    orientationraw = sense.get_orientation_degrees()
+
+    pitch = orientationraw['p']	#change data into a friendlier format
+    roll = orientationraw['r']
+    yaw = orientationraw['y']
+
+    #format results into a 'csv'
+    IMU_results = [
+
+    	accelx,
+    	accely,
+    	accelz,
+    	gyrox,
+    	gyroy,
+    	gyroz,
+    	compassx,
+    	compassy,
+    	compassz,
+    	pitch,
+    	roll,
+    	yaw
 
     ]
 
+#get data from gatherer methods and write it
+def get_data():
+
+	#call gatherer methods
+	enviro_res = get_enviro()
+	IMU_res = get_IMU()
+
+	#put results into a single list
+	results = []
+	results.extend(enviro_res)
+	results.extend(IMU_res)
+
+	#open data.csv and output findings
     with open("data.csv", 'a') as f:
 
         #initializing csv writer object to write to f
         writer = csv.writer(f)
         writer.writerows([results])
 
-
+#loop infinetly
 while True:
 
     get_data()
