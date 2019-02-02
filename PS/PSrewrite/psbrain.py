73c55de6fc0cd30120feb51cf2a2e4dcdd0008a9
ciplc <wookiegraham@gmail.com>
2019-02-01 19:59:06 -0600

    Updated documentation on entire file

--- PS/PSrewrite/psbrain.py
+++ PS/PSrewrite/psbrain.py
@@ -12,6 +12,7 @@ import csv
 import os
 
 
+# Replacement for csvsetup.py, sets headers for a new .csv file
 class CSVSetup:
 
     def setheaders(self):
@@ -34,24 +35,31 @@ class CSVSetup:
         return "Complete"
 
 
+# Writes data to a csv file in a preset format seen below
 class DataWrite:
+
     def __init__(self):
         self.sense = SenseHat()
         self.sense.set_imu_config(True, True, True)
 
     def get_data(self):
-        """Gets data from environmental sensors and IMU sensors and formats it for writing to a CSV"""
+        """Gets data from environmental sensors and IMU sensors
+
+        and formats it for writing to a CSV with time as the first item
+        """
         # get environmental data from the sensehat
         def get_enviro():
             """Gets environmental data and formats it in the form:
 
             pressure, temperature_pressure, temperature_humidity, humidity
             """
+            # Get readings from each sensor
             pressure = self.sense.get_pressure()
             temp_press = self.sense.get_temperature_from_pressure()
             temp_humid = self.sense.get_temperature_from_humidity()
             humidity = self.sense.get_humidity()
 
+            # Format the readings
             enviro_results = [
                 pressure, temp_press,
                 temp_humid, humidity
@@ -72,6 +80,7 @@ class DataWrite:
             compassraw = self.sense.get_compass_raw()
             orientationraw = self.sense.get_orientation_degrees()
 
+            # Format raw data into a usable list
             imu_results = [
                 accelraw['x'], accelraw['y'], accelraw['z'],
                 gyroraw['x'], gyroraw['y'], gyroraw['z'],
@@ -82,6 +91,7 @@ class DataWrite:
 
             return imu_results
 
+        # Get data from sensors and add time then append together
         enviro_res = get_enviro()
         imu_res = get_imu()
         time = datetime.datetime.now()
@@ -92,61 +102,67 @@ class DataWrite:
         return results
 
     def write_data(self):
-
+        """Writes data to data.csv in append mode as to not delete headers or previous data"""
         with open('data.csv', 'a') as f:
             writer = csv.writer(f)
             writer.writerows(self.get_data())
 
 
+# Camera class which aids in the basic operations of the picamera
 class Camera:
+
     def __init__(self):
         self.camera = picamera.PiCamera()
 
     def read_run(self):
-
+        """Reads the number of runs from runnum.json in order to have file extensions in an ordered fashion"""
         with open('runnum.json') as f:
             data = json.loads(f.read())
             return data['runs']
 
     def dump_run(self):
-
+        """Dumps the number of runs plus one into runnum.json"""
         data = {'runs': self.read_run() + 1}
         with open('runum.json', 'w') as f:
             json.dump(data, f)
 
     def stop_test(self):
-
+        """Tests to see if a file with the extension of .varstp is in the toplevel directory"""
+        # If there is a file in the running dir with a .varstp extension stop recording
         for file in os.listdir('./'):
             if file.endswith('.varstp'):
                 self.stop_recording()
 
     def start_recording(self):
-
+        """Starts the picamera recording with the file format of run_runnum.h264"""
         self.camera.start_recording('run_' + self.read_run() + '.h264')
 
     def stop_recording(self):
-
+        """Stops recording from the picamera and updates the runnum with dump_run"""
         self.camera.stop_recording()
         self.dump_run()
 
 
 def main():
 
+    # Defining objects to work with
     camera = Camera()
     csvsetup = CSVSetup()
 
+    # If the arg in place two is n set headers up
     if sys.argv[2] == 'n':
         stat = csvsetup.setheaders()
         if stat == "Complete":
             # TODO implement RTC loop
             pass
-    elif sys.argv[2] == 'a':
+    elif sys.argv[2] == 'a':  # If arg in place two is a just append
         # TODO implement RTC loop
         pass
-    else:
+    else:  # If neither n nor a is set fail out
         print("Error command line arguments are limited to a or n")
         exit(0)
 
+    # Set camera resolution and start recording
     camera.camera.resolution(1920, 1080)
     camera.start_recording()
 
