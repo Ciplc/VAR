07fe81095b8f79d51fc92bb219350f798b0298af
Henry Graham <wookiegraham@gmail.com>
2019-04-11 10:52:17 -0500

    Tested port getting method, works flawlessly. Checked serial_open also works well. Removed pointless method

--- padctrl/padctrl4/src/backend/serial.java
+++ padctrl/padctrl4/src/backend/serial.java
@@ -9,18 +9,23 @@ import com.fazecast.jSerialComm.SerialPort;
 public class serial {
 
     //Don't really need this but we can keep it here as a placeholder for when we do need it
-    public serial(){
+    private serial(){
         String toWrite;
     }
 
-    //Bunk method, doesn't work shouldn't work
+    //Bunk method, doesn't work, should work
     private void serial_open(){
 
         SerialPort cp = SerialPort.getCommPorts()[0];
         if(cp.openPort()) {
             System.out.println("Port is Open");
+            System.out.println(cp);
         }else {
             System.out.println("Failed to open port");
+        }
+
+        if(cp.closePort()){
+            System.out.println(cp + " has been closed");
         }
 
     }
@@ -37,38 +42,10 @@ public class serial {
         }
     }
 
-    //Check everydevice up to 63, was just put in as a proof of concept should never be used
-    private void port_test(){
-
-        /*SerialPort cp = SerialPort.getCommPort("/dev/tty50");
-        if (cp.openPort()){
-            System.out.println("Port is open");
-        } else {
-
-            System.out.println("Failed to open port");
-        }
-        */
-        try {
-            for (int i = 0; i <= 63; i++) {
-                SerialPort cp = SerialPort.getCommPort("/dev/tty" + i);
-                if (cp.openPort()) {
-                    System.out.println("Port is open");
-                } else {
-
-                    System.out.println("Failed to open port "+ i);
-                }
-            }
-            //Can't fail if you can't not succeed *big brain*
-        } catch(Exception e){
-
-        }
-    }
-
     // Starts this mess up, not perm. just for testing purposes
     public static void main(String[] args){
         serial s = new serial();
-        //s.serial_open();
+        s.serial_open();
         s.get_ports();
-        //s.port_test();
     }
 }
