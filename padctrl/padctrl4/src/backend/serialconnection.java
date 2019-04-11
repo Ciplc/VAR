62e9294751212a1c930f3b506e8fb6b6edd1dd77
Henry Graham <wookiegraham@gmail.com>
2019-04-11 14:17:41 -0500

    Removed old serial class and added new updated serialconnection class in its place, functions nominally testing is needed

---
+++ padctrl/padctrl4/src/backend/serialconnection.java
@@ -0,0 +1,52 @@
+package backend;
+/*Written by Henry G for VAR
+This class is the core for serial communications to an arduino, manging sending data and and connecting.
+ */
+import com.fazecast.jSerialComm.SerialPort;
+
+public class serialconnection{
+
+    private SerialPort sp = SerialPort.getCommPorts()[0];
+    public serialconnection(String path_to_device){
+
+        //SerialPort sp = SerialPort.getCommPort(path_to_device);
+
+        //check if the port is open, if so configure port to liking
+        if(sp.openPort()){
+            System.out.println("Port opened on " + sp.getSystemPortName());
+            sp.setComPortParameters(9600, 8, 1,0);
+            sp.setComPortTimeouts(SerialPort.TIMEOUT_WRITE_BLOCKING, 0, 0);
+        }else{
+            System.out.println("Error, port not opened");
+        }
+    }
+
+    //Write byte[] to serial
+    public void serial_write(Integer message){
+        try{
+            sp.getOutputStream().write(message.byteValue());
+            sp.getOutputStream().flush();
+            System.out.println("Wrote " + message.byteValue());
+        } catch(Exception IOException){
+            System.out.println("Exception in IOException");
+        }
+
+    }
+
+    //Close port after use
+    public void close_port(){
+        if(sp.closePort()){
+            System.out.println("Port closed");
+        }else{
+            System.out.println("Unable to close port");
+        }
+    }
+
+    //temporary start for testing class
+    public static void main(String[] args){
+        Integer test = 11;
+        serialconnection s = new serialconnection("/dev/tty50");
+        s.serial_write(test);
+        s.close_port();
+    }
+}
