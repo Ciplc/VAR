3a09cc107ca2beb52cefe1e91656fd2cfbf7ab7b
Henry Graham <wookiegraham@gmail.com>
2019-04-11 11:03:07 -0500

    Added method for opening a supplied port by the user, unable to test yet as an arduino is needed

--- padctrl/padctrl4/src/backend/serial.java
+++ padctrl/padctrl4/src/backend/serial.java
@@ -30,8 +30,7 @@ public class serial {
 
     }
 
-    //Should return all serial ports on device, does not work on linux box
-    //needs to be tested on OSX since that is the deployment target
+    //Should return all serial ports on device, does not work on linux box, works on mbp(13,3)
     private void get_ports(){
         //Works in old SerialComm lib but not this one for some reason
         SerialPort cp[] = SerialPort.getCommPorts();
@@ -39,6 +38,20 @@ public class serial {
         for(SerialPort port : cp){
             System.out.println(port.getSystemPortName());
 
+        }
+    }
+
+    //Open a serial port on a user supplied port
+    //Eventually return a string to be displayed on a confirmation frame
+    private void open_supplied(String path_to_device){ //path_to_device = p_t_d
+
+        //Get Serial connection
+        SerialPort cp = SerialPort.getCommPort(path_to_device);
+        //Open serial connection, if successful tell the user
+        if(cp.openPort()){
+            System.out.println("Port is open on " + cp); //Instead of cp possibly use p_t_d, but cp may warn of errors sooner
+        }else{
+            System.out.println("Port did not open on " + path_to_device); //cp will be dead so must use p_t_d
         }
     }
 
