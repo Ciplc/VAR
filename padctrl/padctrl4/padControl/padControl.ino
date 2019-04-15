47b54399946a648bd3a1b4eaf97ed0af23ebd592
ciplc <wookiegraham@gmail.com>
2019-04-14 21:59:00 -0500

    Setup placeholder for arduino program w/ header

---
+++ padctrl/padctrl4/padControl/padControl.ino
@@ -0,0 +1,12 @@
+/*
+Developed by Henry G for the VAR pad control project
+
+Pad control program designed to take inputs over serial then perform the corrisponding task
+*/
+void setup(){
+
+}
+
+void loop(){
+
+}
