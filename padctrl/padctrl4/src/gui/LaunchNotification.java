59ef1ef930b97eed7ce31bed73db6cef680d7c5a
Henry Graham <wookiegraham@gmail.com>
2019-04-18 08:32:43 -0500

    Added basic template for timer frame and updated notes accordingly

--- padctrl/padctrl4/src/gui/LaunchNotification.java
+++ padctrl/padctrl4/src/gui/LaunchNotification.java
@@ -7,6 +7,13 @@ public class LaunchNotification extends Template implements ActionListener {
     public LaunchNotification(){
 
         this.setTitle("Launched");
+        this.setBounds(100,50,700,160);
+        
+    }
+
+    //For testing only
+    public static void main(String[] args){
+        LaunchNotification ln = new LaunchNotification();
 
     }
 }
---
+++ padctrl/padctrl4/src/gui/TimerFrame.java
@@ -0,0 +1,15 @@
+package gui;
+
+import java.awt.event.ActionListener;
+
+public class TimerFrame extends Template implements ActionListener {
+
+    public TimerFrame(){
+
+    }
+
+    //Testing only
+    public static void main(String[] args){
+        
+    }
+}
