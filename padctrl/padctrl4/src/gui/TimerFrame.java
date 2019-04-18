469e8f8573cd46570f5af45fc1c86ceec8209a1b
Henry Graham <wookiegraham@gmail.com>
2019-04-18 10:36:00 -0500

    Added base for timer frame

--- padctrl/padctrl4/src/gui/TimerFrame.java
+++ padctrl/padctrl4/src/gui/TimerFrame.java
@@ -1,5 +1,7 @@
 package gui;
-
+/*
+Frame which counts down time given by input and also counts up time after the fact
+ */
 import java.awt.event.ActionListener;
 
 public class TimerFrame extends Template implements ActionListener {
@@ -10,6 +12,6 @@ public class TimerFrame extends Template implements ActionListener {
 
     //Testing only
     public static void main(String[] args){
-        
+
     }
 }
