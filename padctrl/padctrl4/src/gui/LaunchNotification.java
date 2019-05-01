290395095310f45f69b38314ef9086aa20febd16
Henry Graham <wookiegraham@gmail.com>
2019-05-01 10:55:39 -0500

    Finished LaunchNotification

--- padctrl/padctrl4/src/gui/LaunchNotification.java
+++ padctrl/padctrl4/src/gui/LaunchNotification.java
@@ -1,14 +1,38 @@
 package gui;
 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class LaunchNotification extends Template implements ActionListener {
 
+    private JPanel conformationPanel = new JPanel(new BorderLayout());
+    private JLabel launchLabel = new JLabel("Launched",SwingConstants.CENTER);
+
     public LaunchNotification(){
 
+        //Formatting window
         this.setTitle("Launched");
-        this.setBounds(100,50,700,160);
+        this.setBounds(500,400,500,160);
+
+        //Configure launch label
+        launchLabel.setForeground(Color.WHITE);
+
+        //Configure conformation panel
+        conformationPanel.add(launchLabel,BorderLayout.CENTER);
+        conformationPanel.setBackground(BACKGROUND_COLOR);
+
+        //Modify nav panel
+        navigationButtonPanel.remove(nextButton);
+        navigationButtonPanel.remove(returnButton);
+
+        //Modifying button panel
+        buttonPanel.remove(navigationButtonPanel);
+        buttonPanel.add(navigationButtonPanel,BorderLayout.CENTER);
         
+        //Add Components and set visible
+        this.add(conformationPanel,BorderLayout.CENTER);
+        this.setVisible(true);
     }
 
     //For testing only
