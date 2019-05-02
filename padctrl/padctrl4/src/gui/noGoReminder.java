1b4ecb80840ec7ab96f1495f89a6107512a080c7
Henry Graham <wookiegraham@gmail.com>
2019-05-02 10:48:22 -0500

    trying to fix files

--- padctrl/padctrl4/src/gui/noGoReminder.java
+++ padctrl/padctrl4/src/gui/noGoReminder.java
@@ -1,64 +1,83 @@
 package gui;
 /*
-Written by Henry G and An T for var padctrl 5/2/2019
+Written by Henry G for var padctrl 5/1/2019
 NoGo reminder for status of various depots
  */
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
-public class noGoReminder extends Template implements ActionListener {
+public class NoGoReminder extends Template implements ActionListener {
 
 
     //Constants
     private final Color NO_GO_COLOR = new Color(255,0,0);
 
     //Declaring Objects
-    private JPanel textPanel = new JPanel(new BorderLayout());
-    private JLabel warningLabel = new JLabel("ALL POSITIONS ARE NOT GO FOR "
-            + "LAUNCH!",SwingConstants.CENTER);
+    private JPanel boosterPanel = new JPanel(new BorderLayout());
+    private JPanel guidancePanel = new JPanel(new BorderLayout());
+    private JPanel rangePanel = new JPanel(new BorderLayout());
+    private JPanel flightPanel = new JPanel(new BorderLayout());
+    private JPanel compositePanel = new JPanel((new FlowLayout()));
 
-    public noGoReminder(){
+    private JLabel boosterNoLabel = new JLabel("Booster No");
+    private JLabel guidanceNoLabel = new JLabel("Guidance No");
+    private JLabel rangeNoLabel = new JLabel("Range No");
+    private JLabel flightNoLabel = new JLabel("Flight No");
+
+
+    public NoGoReminder(){
 
         //Formatting
         this.setTitle("No Go Reminder");
         this.setBounds(500,400,500,160);
-        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        
-        //Formatting labels
-        warningLabel.setFont(WARNING_FONT);
-        warningLabel.setForeground(Color.WHITE);
 
-        //Formatting textPanel
-        textPanel.add(warningLabel, BorderLayout.CENTER);
-        textPanel.setBackground(NO_GO_COLOR);
+        //Booster Panel
+        boosterPanel.add(boosterNoLabel, BorderLayout.EAST);
+        boosterPanel.setBackground(NO_GO_COLOR);
+
+        //Guidance Panel
+        guidancePanel.add(guidanceNoLabel, BorderLayout.EAST);
+        guidancePanel.setBackground(NO_GO_COLOR);
+
+        //Range Panel
+        rangePanel.add(rangeNoLabel, BorderLayout.EAST);
+        rangePanel.setBackground(NO_GO_COLOR);
+
+        //Flight Panel
+        flightPanel.add(flightNoLabel, BorderLayout.EAST);
+        flightPanel.setBackground(NO_GO_COLOR);
+
+        //Composite Panel
+        compositePanel.add(boosterPanel);
+        compositePanel.add(guidancePanel);
+        compositePanel.add(rangePanel);
+        compositePanel.add(flightPanel);
+        compositePanel.setBackground(BACKGROUND_COLOR);
 
         //Modify nav panel
         navigationButtonPanel.remove(nextButton);
-        navigationButtonPanel.remove(exitButton);
-        navigationButtonPanel.setBackground(NO_GO_COLOR);
+        navigationButtonPanel.remove(returnButton);
 
         //Modifying button panel
         buttonPanel.remove(navigationButtonPanel);
         buttonPanel.add(navigationButtonPanel, BorderLayout.CENTER);
 
         //Adding to frame and setting visible
-        this.add(textPanel);
+        this.add(compositePanel);
 
         this.setVisible(true);
     }
 
     //Testing Only
     public static void main(String[] args) {
-        noGoReminder ngr = new noGoReminder();
+        NoGoReminder ngr = new NoGoReminder();
 
     }
 
     /*@Override
     public void actionPerformed(ActionEvent e){
-
         String command = e.getActionCommand();
-
         if(command.equals("Exit Application")){
             this.dispose();
         }
