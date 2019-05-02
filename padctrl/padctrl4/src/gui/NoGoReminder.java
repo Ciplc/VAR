4c6724ca82887563cb05bb2beeafe1b66c4bba4b
Henry Graham <wookiegraham@gmail.com>
2019-05-02 08:11:30 -0500

    Renamed NoGoReminder and added a base for actionEvent changes

--- padctrl/padctrl4/src/gui/NoGoReminder.java
+++ padctrl/padctrl4/src/gui/NoGoReminder.java
@@ -6,24 +6,27 @@ NoGo reminder for status of various depots
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
-import logging.data;
 
-public class noGoReminder extends Template implements ActionListener {
+public class NoGoReminder extends Template implements ActionListener {
+
+
+    //Constants
+    private final Color NO_GO_COLOR = new Color(255,0,0);
 
     //Declaring Objects
     private JPanel boosterPanel = new JPanel(new BorderLayout());
     private JPanel guidancePanel = new JPanel(new BorderLayout());
     private JPanel rangePanel = new JPanel(new BorderLayout());
     private JPanel flightPanel = new JPanel(new BorderLayout());
-    private JPanel compositePanel = new JPanel((new BorderLayout()));
+    private JPanel compositePanel = new JPanel((new FlowLayout()));
 
     private JLabel boosterNoLabel = new JLabel("Booster No");
-    private JLabel guidanceNoLable = new JLabel("Guidance No");
+    private JLabel guidanceNoLabel = new JLabel("Guidance No");
     private JLabel rangeNoLabel = new JLabel("Range No");
     private JLabel flightNoLabel = new JLabel("Flight No");
 
 
-    public noGoReminder(){
+    public NoGoReminder(){
 
         //Formatting
         this.setTitle("No Go Reminder");
@@ -31,21 +34,26 @@ public class noGoReminder extends Template implements ActionListener {
 
         //Booster Panel
         boosterPanel.add(boosterNoLabel, BorderLayout.EAST);
+        boosterPanel.setBackground(NO_GO_COLOR);
 
         //Guidance Panel
-        guidancePanel.add(guidanceNoLable, BorderLayout.EAST);
+        guidancePanel.add(guidanceNoLabel, BorderLayout.EAST);
+        guidancePanel.setBackground(NO_GO_COLOR);
 
         //Range Panel
         rangePanel.add(rangeNoLabel, BorderLayout.EAST);
+        rangePanel.setBackground(NO_GO_COLOR);
 
         //Flight Panel
         flightPanel.add(flightNoLabel, BorderLayout.EAST);
+        flightPanel.setBackground(NO_GO_COLOR);
 
         //Composite Panel
         compositePanel.add(boosterPanel);
         compositePanel.add(guidancePanel);
         compositePanel.add(rangePanel);
         compositePanel.add(flightPanel);
+        compositePanel.setBackground(BACKGROUND_COLOR);
 
         //Modify nav panel
         navigationButtonPanel.remove(nextButton);
@@ -63,7 +71,17 @@ public class noGoReminder extends Template implements ActionListener {
 
     //Testing Only
     public static void main(String[] args) {
-        noGoReminder ngr = new noGoReminder();
+        NoGoReminder ngr = new NoGoReminder();
 
     }
+
+    /*@Override
+    public void actionPerformed(ActionEvent e){
+
+        String command = e.getActionCommand();
+
+        if(command.equals("Exit Application")){
+            this.dispose();
+        }
+    }*/
 }
