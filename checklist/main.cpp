ee1abf93677804ad1104b136226a96415ca2fb2e
ciplc <wookiegraham@gmail.com>
2019-06-28 00:27:37 -0500

    added the base for checklist

---
+++ checklist/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string lifterName;
+string date;
+string goal;
+double dryMass;
+double wetMass;
+double expectedAltitude;
+double actualAltitude;
+
+// Write contents passed to it to a file
+void writeToFile(){
+
+
+}
+
+int main(){
+    
+}
